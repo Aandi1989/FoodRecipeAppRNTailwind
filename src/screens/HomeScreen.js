59cd8a38a78de1e10c96c5d475122087b40b49a0
Aandi1989 <navaliny1989@gmail.com>
2023-09-17 16:29:46 +0300

    fetching categories from API

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,78 @@
-import { View, Text, SafeAreaView } from 'react-native';
-import React from 'react';
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { BellIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import Categories from '../components/categories';
+import axios from 'axios';
+
 
 export default function HomeScreen() {
+
+  const [activeCategoty, setActiveCategory] = useState('Beef');
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories();
+  },[])
+
+  const getCategories = async () => {
+    try{
+      const response = await axios.get('https://themealdb.com/api/json/v1/1/categories.php');
+      // console.log('got categories: ', response.data);
+      if(response && response.data){
+        setCategories(response.data.categories);
+      }
+    }catch(err){
+      console.log('error: ',err.message);
+    }
+  }
   return (
-    <View className="flex-1 flex-col items-center justify-center">
-      <Text className="text-4xl">HomeScreen</Text>
+    <View className="flex-1 bg-white">
       <StatusBar style="dark"/>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 50}}
+        className="space-y-6 pt-14"
+        >
+          {/* avatar and bell icon */}
+          <View className="mx-4 flex-row justify-between items-center mb-2">
+            <Image source={require('../../assets/images/avatar.png')} 
+            style={{height: hp(5), width: hp(5.5)}}/>
+            <BellIcon size={hp(4)} color="gray" />
+          </View>
+
+          {/* greetings and punchline */}
+          <View className="mx-4 space-y-2 mb-2 ">
+            <Text style={{fontSize: hp(1.7)}} className="text-neutral-600">Hello, Alex!</Text>
+            <View>
+              <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600">Make your own food, </Text>
+            </View>
+            <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600">
+              stay at <Text className="text-amber-400">home</Text>
+            </Text>
+          </View>
+
+          {/* search bar */}
+          <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+            <TextInput
+              placeholder='Search any recipe'
+              placeholderTextColor={'gray'}
+              style={{fontSize: hp(1.7)}}
+              className="flex-1 text-base mb-1 pl-3 tracking-wider"
+            />
+            <View className="bg-white rounded-full p-3">
+              <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color="gray" />
+            </View>
+          </View>
+
+          {/* categories */}
+          <View>
+            { categories.length>0 && <Categories categories={categories} activeCategoty={activeCategoty} setActiveCategory={setActiveCategory}/>}
+          </View>
+      </ScrollView>
     </View>
   )
 }
